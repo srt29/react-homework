14bf2c8294c3c70707ad6cfd6d1a3d63166b8b19
srt29 <143508194+srt29@users.noreply.github.com>
2024-03-10 12:04:40 +0200

    Added link to github respository

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Weather from "./Weather";
 import axios from "axios";
+
 import './App.css';
 
 export default function App() {
@@ -7,6 +8,9 @@ export default function App() {
     <div className="App">
      <h1>Weather App</h1>
      <Weather />
+     <p>
+      <a href="https://github.com/srt29/react-homework">Open-source code</a> by <a href="https://github.com/srt29">Sian Thumbadoo</a>
+     </p>
     </div>
   );
 }
